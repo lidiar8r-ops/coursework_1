4d42775c593af3d3cace90210022cda6a720373c
Рыбина Лидия <lidiar@rambler.ru>
2025-11-02 15:56:00 +0500

    update project

--- src/reports.py
+++ src/reports.py
@@ -6,7 +6,16 @@ def spending_by_weekday(transactions: pd.DataFrame,
                         date: Optional[str] = None) -> pd.DataFrame:
 
     # фильтруем по периоду и статусу операции <<OK>> в результат result_list
+    # вычисляем период
+    if date is None:
+        date = "текущая"
+    range_data_from = date -90 дней
+    range_data_to = date
 
+    # фильтруем по периоду и статусу операции <<OK>> в результат result_list
+
+    # полученный список передаем в функцию с днем недели, где фильтруем и получаем среднее значение по дню
+    # формируем  вывод в файл/консоль
     pass
 
 
