2fb2b22e28aba57738d01327828c631f76f478f0
Рыбина Лидия <lidiar@rambler.ru>
2025-11-05 20:40:37 +0500

    update test_reports

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -37,3 +37,45 @@ def test_with_valid_date(test_transactions):
         print("Нет данных для расчета!")
     else:
         assert not result.empty
+
+
+
+def test_positive_scenario(test_transactions):
+    """
+    Проверка нормального функционирования при наличии расходов и правильной фильтрации.
+    """
+    valid_date = '01.10.2023'
+    result = spending_by_weekday(test_transactions, date=valid_date)
+    assert isinstance(result, pd.DataFrame)
+    if result.empty:
+        print("Нет данных для расчета!")
+    else:
+        assert not result.empty
+
+
+def test_no_expense_transactions():
+    """
+    Проверка случая, когда нет транзакций с расходами.
+    """
+    no_expense_data = pd.DataFrame({
+        'Дата платежа': pd.to_datetime(['2023-09-01']),
+        'Сумма': [100],
+        'Валюта': ['USD']
+    })
+    result = spending_by_weekday(no_expense_data)
+    assert isinstance(result, pd.DataFrame)
+    assert result.empty
+
+
+def test_empty_after_filtering():
+    """
+    Проверка случая, когда транзакции есть, но они находятся вне нужного временного диапазона.
+    """
+    out_of_range_data = pd.DataFrame({
+        'Дата платежа': pd.to_datetime(['2023-06-01']),
+        'Сумма': [-100],
+        'Валюта': ['USD']
+    })
+    result = spending_by_weekday(out_of_range_data, date='01.10.2023')
+    assert isinstance(result, pd.DataFrame)
+    assert result.empty
