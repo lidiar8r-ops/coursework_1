114470d29740a4840e8c0726b3d5429e526c8c17
Рыбина Лидия <lidiar@rambler.ru>
2025-11-08 03:18:24 +0500

    update readme

--- src/utils.py
+++ src/utils.py
@@ -18,12 +18,8 @@ from src.config import DATA_DIR, LIST_OPERATION, URL_EXCHANGE, URL_EXCHANGE_SP_5
 logger = app_logger.get_logger("utils.log")
 
 
-# Загрузка переменных из .env-файла,3
+# Загрузка переменных из .env-файла
 load_dotenv()
-# headers = {"apikey": os.getenv("API_KEY")}
-# headers_sp_500 = {
-#     "apikey": os.getenv("API_KEY_SP_500"),
-# }
 
 
 def get_list_operation(
